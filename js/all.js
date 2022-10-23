3d3ea347de784d9e494b1cb71ee0d54f66f1ae5d
tomy5566 <buy855341buy@gmail.com>
2022-10-23 16:10:40 +0800

    大更新V2

--- js/all.js
+++ js/all.js
@@ -4,13 +4,13 @@
 // //來自https://bobbyhadz.com/blog/javascript-import-json-file
 // //注意有動到HTML 增加
 import jsonData from '../KHtravelJsonUse.json' assert {type: 'json'};
-console.log('jsonData', jsonData);
+// console.log('jsonData', jsonData);
 // const parsedData = JSON.parse(jsonData);
 let travelData = jsonData.result.records;
-console.log('travelData', travelData);
-console.log('travelData[0].Name', travelData[0].Name);
-console.log('travelData.length', travelData.length);
-// console.log('travelData.Zone', travelData[0].Zone); //測試
+// console.log('travelData', travelData);                 //測試
+// console.log('travelData[0].Name', travelData[0].Name); //測試
+// console.log('travelData.length', travelData.length);   //測試
+// console.log('travelData.Zone', travelData[0].Zone);    //測試
 
 // 建立 區(Zone) 的清單
 
@@ -23,9 +23,9 @@ function FindZone() {
     mySet.add(travelData[i].Zone);   //不要重複的
   }
   
-  console.log('mySet1',mySet);
+  // console.log('mySet1',mySet);
   let myArray = Array.from(mySet);     //把set轉成陣列
-  console.log('myArray',myArray);
+  // console.log('myArray',myArray);   //測試
   return myArray;
 }
 
@@ -137,12 +137,12 @@ function updatecardlist() {
   // var strphoto ="";
   // var strInfo = "";
   for (var i = 0; i < travelData.length; i++) {
-      const strphoto = '<div class="cardphoto" style="background-image: url(' + travelData[i].Picture1 + ')">'+'<h2 class="cardname"> 景點:' + travelData[i].Name +'</h2></div>';
-      const strInfo = '<div class="cardinfo"> '+ '<ul><li>地址:' +travelData[i].Add +'</li>'+' <li>電話:' +travelData[i].Tel +'</li></ul></div>';
+    const strphoto = '<div class="cardphoto" style="background-image: url(' + travelData[i].Picture1 + ')">'+'<div class="zoneflex"><h2 class="cardname">' + travelData[i].Name +'</h2><p>'+ travelData[i].Zone+'</p></div></div>';
+      const strInfo = '<div class="cardinfo"> '+ '<ul><li><img src="../images/icons_clock.png" alt="開放時間"> 開放時間:'+ travelData[i].Opentime +'</li>'+ '<li><img src="../images/icons_pin.png" alt="地址"> 地址:' +travelData[i].Add +'</li>'+' <li><img src="../images/icons_phone.png" alt="電話">&nbsp;&nbsp;電話:' +travelData[i].Tel +'</li><li class="Ticket"><img src="../images/icons_tag.png" alt="收費">&nbsp;&nbsp;' +travelData[i].Ticketinfo +'&nbsp;&nbsp;</li></ul></div>';
       const newCard = '<div class="card-wrapper">' + strphoto + strInfo + '</div>'; 
       strCardWrapper = strCardWrapper + newCard; 
   }
-  console.log(strCardWrapper);
+  // console.log(strCardWrapper);
   // cardname.innerHTML = str;
   card.innerHTML = strCardWrapper;
 
@@ -159,15 +159,23 @@ updatecardlist();
 zonelist.addEventListener('change', function (e) {
   let strCardWrapper = '';     //不可以放在外面，否則會無限疊加
   let select = e.target.value;
-  console.log('select', select)
+  // console.log('select', select)
   var strInfo = '';
   for(let i=0;i<travelData.length;i++){
       if(select == travelData[i].Zone){
-        const strphoto = '<div class="cardphoto" style="background-image: url(' + travelData[i].Picture1 + ')">'+'<h2 class="cardname"> 景點:' + travelData[i].Name +'</h2></div>';
-        const strInfo = '<div class="cardinfo"> '+ '<ul><li>地址:' +travelData[i].Add +'</li>'+' <li>電話:' +travelData[i].Tel +'</li></ul></div>';
+        const strphoto = '<div class="cardphoto" style="background-image: url(' + travelData[i].Picture1 + ')">'+'<div class="zoneflex"><h2 class="cardname">' + travelData[i].Name +'</h2><p>'+ travelData[i].Zone+'</p></div></div>';
+        const strInfo = '<div class="cardinfo"> '+ '<ul><li><img src="../images/icons_clock.png" alt="開放時間"> 開放時間:'+ travelData[i].Opentime +'</li>'+ '<li><img src="../images/icons_pin.png" alt="地址"> 地址:' +travelData[i].Add +'</li>'+' <li><img src="../images/icons_phone.png" alt="電話">&nbsp;&nbsp;電話:' +travelData[i].Tel +'</li><li class="Ticket"><img src="../images/icons_tag.png" alt="收費">&nbsp;&nbsp;' +travelData[i].Ticketinfo +'&nbsp;&nbsp;</li></ul></div>';
         const newCard = '<div class="card-wrapper">' + strphoto + strInfo + '</div>'; 
         strCardWrapper = strCardWrapper + newCard; 
-      }        
+      }       
+      
+      else if(select == '全高雄各區'){
+        const strphoto = '<div class="cardphoto" style="background-image: url(' + travelData[i].Picture1 + ')">'+'<div class="zoneflex"><h2 class="cardname">' + travelData[i].Name +'</h2><p>'+ travelData[i].Zone+'</p></div></div>';
+        const strInfo = '<div class="cardinfo"> '+ '<ul><li><img src="../images/icons_clock.png" alt="開放時間"> 開放時間:'+ travelData[i].Opentime +'</li>'+ '<li><img src="../images/icons_pin.png" alt="地址"> 地址:' +travelData[i].Add +'</li>'+' <li><img src="../images/icons_phone.png" alt="電話">&nbsp;&nbsp;電話:' +travelData[i].Tel +'</li><li class="Ticket"><img src="../images/icons_tag.png" alt="收費">&nbsp;&nbsp;' +travelData[i].Ticketinfo +'&nbsp;&nbsp;</li></ul></div>';
+        const newCard = '<div class="card-wrapper">' + strphoto + strInfo + '</div>'; 
+        strCardWrapper = strCardWrapper + newCard; 
+      } 
+
   } 
   card.innerHTML = strCardWrapper;
   zonename.innerHTML = select;
@@ -187,3 +195,55 @@ zonelist.addEventListener('change', function (e) {
 // } 
 
 
+
+
+//監聽中間的按鈕
+let menu = document.querySelector('.menu');
+menu.addEventListener('click', function (e) {
+  console.log(e);
+  console.log(e.target);
+  console.log(e.target.nodeName);
+  console.log(e.target.className);
+  if(e.target.nodeName == 'INPUT'){
+    let strCardWrapper ='';
+
+    for(let i=0;i<travelData.length;i++){
+      
+      if(e.target.defaultValue == travelData[i].Zone){
+        
+        const strphoto = '<div class="cardphoto" style="background-image: url(' + travelData[i].Picture1 + ')">'+'<div class="zoneflex"><h2 class="cardname">' + travelData[i].Name +'</h2><p>'+ travelData[i].Zone+'</p></div></div>';
+        const strInfo = '<div class="cardinfo"> '+ '<ul><li><img src="../images/icons_clock.png" alt="開放時間"> 開放時間:'+ travelData[i].Opentime +'</li>'+ '<li><img src="../images/icons_pin.png" alt="地址"> 地址:' +travelData[i].Add +'</li>'+' <li><img src="../images/icons_phone.png" alt="電話">&nbsp;&nbsp;電話:' +travelData[i].Tel +'</li><li class="Ticket"><img src="../images/icons_tag.png" alt="收費">&nbsp;&nbsp;' +travelData[i].Ticketinfo +'&nbsp;&nbsp;</li></ul></div>';
+        const newCard = '<div class="card-wrapper">' + strphoto + strInfo + '</div>'; 
+        strCardWrapper = strCardWrapper + newCard; 
+      }        
+  } 
+  card.innerHTML = strCardWrapper;
+  zonename.innerHTML = e.target.defaultValue  ;
+  }
+
+}, false);
+
+
+
+
+
+
+
+// switch (e.target.className) {
+//   case 'bt1':{
+//     alert('bt1');
+//     break;
+//   }
+//   case 'bt2':{
+//     alert('bt2');
+//     break;
+//   }
+//   case 'bt3':{
+//     alert('bt3');
+//     break;
+//   }
+//   case 'bt4':{
+//     alert('bt4');
+//     break;
+//   }
+// alert('qqq');
